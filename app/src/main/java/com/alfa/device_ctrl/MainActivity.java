96a7b534fa4988af3776865d5dfb69c44d0448d4
Alfa Device Ctrl <alfa-device-ctrl@localhost>
2026-08-22 15:52:11 +0000

    feat: initialize android application skeleton

---
+++ app/src/main/java/com/alfa/device_ctrl/MainActivity.java
@@ -0,0 +1,11 @@
+package com.alfa.device_ctrl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public final class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
