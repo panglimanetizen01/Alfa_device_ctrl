026a045d9b9d6d5de79c67871bfdedac352ea4ce
panglimanetizen01 <rajjah525@gmail.com>
2026-09-09 23:50:10 +0700

    fix: pin accessible immutable Debian OCI rootfs layer

--- app/src/main/java/com/alfa/device_ctrl/RuntimeProfile.java
+++ app/src/main/java/com/alfa/device_ctrl/RuntimeProfile.java
@@ -4,9 +4,9 @@ package com.alfa.device_ctrl;
 public final class RuntimeProfile {
     public static final String ID = "debian";
     public static final String DISPLAY_NAME = "Debian";
-    public static final String ROOTFS_URL = "https://github.com/debuerreotype/docker-debian-artifacts/raw/fb7215b47dab72bdbdd59204a7b7914311431d90/bookworm/rootfs.tar.xz";
-    public static final String ROOTFS_SHA256 = "202ecca447dbf1b3ac1b1e983d9363381ac6a34f8e22d7d786125d06754ebb76";
-    public static final boolean ROOTFS_GZIP = false;
+    public static final String ROOTFS_URL = "https://github.com/debuerreotype/docker-debian-artifacts/raw/14d91d295c23da6cc04d4bfe8b3d74a8a6c54e5c/bookworm/oci/blobs/rootfs.tar.gz";
+    public static final String ROOTFS_SHA256 = "445be8da0a7289e4b5d70a5c779ad63d484e76aa14fe2ad45893da9eb077e4e8";
+    public static final boolean ROOTFS_GZIP = true;
 
     private RuntimeProfile() { }
 }
