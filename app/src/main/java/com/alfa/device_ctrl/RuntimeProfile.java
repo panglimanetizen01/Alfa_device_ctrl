093f199e9c797f6e1efdbaf0e5cae9f0ffc41fe0
panglimanetizen01 <rajjah525@gmail.com>
2026-09-09 23:52:26 +0700

    fix: correct Debian OCI rootfs layer digest

--- app/src/main/java/com/alfa/device_ctrl/RuntimeProfile.java
+++ app/src/main/java/com/alfa/device_ctrl/RuntimeProfile.java
@@ -5,7 +5,7 @@ public final class RuntimeProfile {
     public static final String ID = "debian";
     public static final String DISPLAY_NAME = "Debian";
     public static final String ROOTFS_URL = "https://github.com/debuerreotype/docker-debian-artifacts/raw/14d91d295c23da6cc04d4bfe8b3d74a8a6c54e5c/bookworm/oci/blobs/rootfs.tar.gz";
-    public static final String ROOTFS_SHA256 = "445be8da0a7289e4b5d70a5c779ad63d484e76aa14fe2ad45893da9eb077e4e8";
+    public static final String ROOTFS_SHA256 = "c6cbf97176c58c741329cd787e932a1e47931b35f5dc0f23db3e6e82924fef0f";
     public static final boolean ROOTFS_GZIP = true;
 
     private RuntimeProfile() { }
