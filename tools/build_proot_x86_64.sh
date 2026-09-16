128062acc9f6d36c9dbd90c882a7453fed2591b8
panglimanetizen01 <rajjah525@gmail.com>
2026-09-16 20:25:37 +0700

    fix: complete talloc cross-answers for x86_64 build

--- tools/build_proot_x86_64.sh
+++ tools/build_proot_x86_64.sh
@@ -50,6 +50,7 @@ Checking for -D_FILE_OFFSET_BITS=64: OK
 Checking for WORDS_BIGENDIAN: OK
 Checking for C99 vsnprintf: OK
 Checking for HAVE_SECURE_MKSTEMP: OK
+rpath library support: OK
 -Wl,--version-script support: FAIL
 Checking correct behavior of strtoll: OK
 Checking correct behavior of strptime: OK
@@ -59,6 +60,10 @@ Checking for HAVE_IFACE_IFREQ: OK
 Checking getconf LFS_CFLAGS: OK
 Checking for large file support without additional flags: OK
 Checking for working strptime: OK
+Checking for HAVE_SHARED_MMAP: OK
+Checking for HAVE_MREMAP: OK
+Checking for HAVE_INCOHERENT_MMAP: OK
+Checking getconf large file support flags work: OK
 EOF
 ./configure --prefix="$TALLOC_PREFIX" --disable-rpath --disable-python --cross-compile --cross-answers=cross-answers.txt \
   CC="$CC --target=x86_64-linux-android26" AR="$AR" RANLIB="$TOOLCHAIN/llvm-ranlib"
