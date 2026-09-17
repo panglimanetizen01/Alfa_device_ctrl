6b7ef08e475c72f56d1723470d3d1a3990b624d9
panglimanetizen01 <rajjah525@gmail.com>
2026-09-17 23:20:54 +0700

    fix: enable RELRO for libtermux

--- terminal-emulator/src/main/jni/Android.mk
+++ terminal-emulator/src/main/jni/Android.mk
@@ -3,4 +3,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE:= libtermux
 LOCAL_SRC_FILES:= termux.c
 LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384 -Wl,-z,common-page-size=16384
+LOCAL_LDFLAGS += -Wl,-z,relro
 include $(BUILD_SHARED_LIBRARY)
